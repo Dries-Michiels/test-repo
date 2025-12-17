96e3833bcd015a8bc768ebb040bb4793aaab94fd
Dries-Michiels <driesmichiels73@gmail.com>
2025-12-17 15:46:51 +0100

    Voeg beroep toe aan persoonsgegevens
    Een nieuwe variabele 'beroep' is toegevoegd en wordt nu weergegeven in de console output naast naam, leeftijd en woonplaats.

--- oef1.mjs
+++ oef1.mjs
@@ -1,12 +1,15 @@
 var Naam;
 var leeftijd;
 var woonplaats;
+var beroep; 
 
 
 Naam = "Jan";
 leeftijd = 25;
 woonplaats = "Amsterdam";
+beroep = "Programmeur";
 
 console.log("Naam: " + Naam);
 console.log("Leeftijd: " + leeftijd);
 console.log("Woonplaats: " + woonplaats);
+console.log("Beroep: " + beroep);
