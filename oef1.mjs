d3ebe0c9fbbf9f8b972c41ccb169ac132dceba08
Dries-Michiels <driesmichiels73@gmail.com>
2025-12-17 15:51:41 +0100

    Add hobby variable and log output
    Introduced a new variable 'Hobby' with the value 'Fotografie' and updated the script to log the hobby to the console.

--- oef1.mjs
+++ oef1.mjs
@@ -2,14 +2,17 @@ var Naam;
 var leeftijd;
 var woonplaats;
 var beroep; 
+var Hobby;
 
 
 Naam = "Jan";
 leeftijd = 25;
 woonplaats = "Amsterdam";
 beroep = "Programmeur";
+Hobby = "Fotografie";
 
 console.log("Naam: " + Naam);
 console.log("Leeftijd: " + leeftijd);
 console.log("Woonplaats: " + woonplaats);
 console.log("Beroep: " + beroep);
+console.log("Hobby: " + Hobby);
