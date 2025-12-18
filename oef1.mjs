55da266d553c1417f15a2303d3ef203a42c92e08
Dries-Michiels <driesmichiels73@gmail.com>
2025-12-18 09:38:25 +0100

    Voeg extra variabele toe en log deze
    Een nieuwe variabele 'extraVar' is toegevoegd en wordt nu gelogd naar de console. Deze wijziging demonstreert het toevoegen en gebruiken van een extra variabele in het script.

--- oef1.mjs
+++ oef1.mjs
@@ -3,6 +3,7 @@ var leeftijd;
 var woonplaats;
 var beroep; 
 var Hobby;
+var extraVar; // Deze variabele is niet gebruikt
 
 
 Naam = "Jan";
@@ -10,9 +11,11 @@ leeftijd = 25;
 woonplaats = "Amsterdam";
 beroep = "Programmeur";
 Hobby = "Fotografie";
+extraVar = "Ongebruikte variabele";
 
 console.log("Naam: " + Naam);
 console.log("Leeftijd: " + leeftijd);
 console.log("Woonplaats: " + woonplaats);
 console.log("Beroep: " + beroep);
 console.log("Hobby: " + Hobby);
+console.log("Extra Variabele: " + extraVar);
