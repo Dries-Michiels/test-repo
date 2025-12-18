facf92e44caadc913e2af40b661a79d7d2c76078
Dries-Michiels <driesmichiels73@gmail.com>
2025-12-18 10:03:01 +0100

    Add unusedVar2 variable and log its value
    Introduced a new unused variable 'unusedVar2', assigned it a value, and added a console.log statement to display its value.

--- oef1.mjs
+++ oef1.mjs
@@ -6,6 +6,7 @@ var Hobby;
 var extraVar; // Deze variabele is niet gebruikt
 var unusedVariable; // Nog een ongebruikte variabele
 var huts;
+var unusedVar2; // Nog een ongebruikte variabele
 
 
 Naam = "Jan";
@@ -16,6 +17,7 @@ Hobby = "Fotografie";
 extraVar = "Ongebruikte variabele";
 unusedVariable = 42; // Deze variabele is ook niet gebruikt
 huts = 5;
+unusedVar2 = 5+3; // Deze variabele is ook niet gebruikt
 
 console.log("Naam: " + Naam);
 console.log("Leeftijd: " + leeftijd);
@@ -25,3 +27,4 @@ console.log("Hobby: " + Hobby);
 console.log("Extra Variabele: " + extraVar);
 console.log("Ongebruikte Variabele: " + unusedVariable);
 console.log("Aantal huts: " + huts);
+console.log("Nog een ongebruikte variabele: " + unusedVar2);
