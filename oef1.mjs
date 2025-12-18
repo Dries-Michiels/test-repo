c7647fd7fded922019168a8f8e162bcd3dec3a82
Dries-Michiels <driesmichiels73@gmail.com>
2025-12-18 11:04:57 +0100

    Add variables for sum calculation and logging
    Introduced getal1, getal2, and getal3 to demonstrate addition and log their values. Added a 'test' variable and corresponding console output for demonstration purposes.

--- oef1.mjs
+++ oef1.mjs
@@ -7,6 +7,10 @@ var extraVar; // Deze variabele is niet gebruikt
 var unusedVariable; // Nog een ongebruikte variabele
 var huts;
 var unusedVar2; // Nog een ongebruikte variabele
+var getal1 = 4;
+var getal2 = 5;
+
+va test;
 
 
 Naam = "Jan";
@@ -18,6 +22,8 @@ extraVar = "Ongebruikte variabele";
 unusedVariable = 42; // Deze variabele is ook niet gebruikt
 huts = 5;
 unusedVar2 = 5+3; // Deze variabele is ook niet gebruikt
+var getal3 = getal1 + getal2;
+test = "yow";
 
 console.log("Naam: " + Naam);
 console.log("Leeftijd: " + leeftijd);
@@ -28,3 +34,8 @@ console.log("Extra Variabele: " + extraVar);
 console.log("Ongebruikte Variabele: " + unusedVariable);
 console.log("Aantal huts: " + huts);
 console.log("Nog een ongebruikte variabele: " + unusedVar2);
+console.log("Getal 1: " + getal1);
+console.log("Getal 2: " + getal2);
+console.log("Som van Getal 1 en Getal 2: " + getal3);
+console.log("Test variabele: " + test);
+
